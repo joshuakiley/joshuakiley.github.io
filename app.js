d50574e136a60193beb85843a173b94806eb0c6d
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-10-15 14:28:15 -0400

    Test API

--- app.js
+++ app.js
@@ -7,8 +7,5 @@ $(() => {
     $.ajax({
         type: "GET",
         url: "https://apifree.forvo.com/action/standard-pronunciation/format/json/word/love/key/50cc8c901d98b50a741856ded1071131",
-        data: {
-
-        }
     }).then(handleData);
 })
