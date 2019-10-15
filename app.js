210c0926986291ad1ecc554929db2b043b12bda2
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-10-15 14:20:34 -0400

    Test API

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(() => {
-    
-    console.log(`https://apifree.forvo.com/action/word-pronunciations/format/json/爱/order/rate-desc/limit/1/key/50cc8c901d98b50a741856ded1071131`);
 
+    const handleData = (data) => {
+        console.log(data);
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "https://apifree.forvo.com/action/standard-pronunciation/format/json/word/love/key/50cc8c901d98b50a741856ded1071131",
+        data: {
+
+        }
+    }).then(handleData);
 })
