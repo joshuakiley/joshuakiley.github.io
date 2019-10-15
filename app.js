93fe6b3f76458f8c8853172fd3aff72f3d056e4b
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-10-15 18:06:16 -0400

    Connect API for pronunciation and API for pinyin

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 $(() => {
 
-    const handleData = (data) => {
-        console.log(data);
+    // const getAudio = (data) => {
+    //     console.log(data.items[0].pathmp3);
+    // }
+
+    // $.ajax({
+    //     // type: "GET",
+    //     url: "https://apifree.forvo.com/key/50cc8c901d98b50a741856ded1071131/format/json/action/standard-pronunciation/word/cat",
+    //     data: "data",
+    //     dataType: "jsonp",
+    // }).then(handleData);
+
+    const getPinYin = (data) => {
+        console.log(data.text);
     }
 
     $.ajax({
         type: "GET",
-        url: "https://apifree.forvo.com/action/standard-pronunciation/format/json/word/love/key/50cc8c901d98b50a741856ded1071131",
-    }).then(handleData);
+        url: "https://api.pinyin.pepe.asia/pinyin/爱",
+        data: "data",
+    }).then(getPinYin);
+
 })
