0e94085ccd34e26c09ea449284ea6c9438e24225
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-10-17 15:48:05 -0400

    Rearrange content and add HOW TO button

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
 $(() => {
-  const getAudio = data => {
-    $(".study-card")
-      .children("button")
-      .remove();
-    const $pronincuationButton = $("<button>")
-      .attr("class", "pronunciaion")
-      .attr("href", `${data.items[0].pathmp3}`)
-      .text(`${data.items[0].word}`);
-    $(".study-card").append($pronincuationButton);
-  };
-
-  $(".pronunciaion").on("click", () => {
-    $.ajax({
-      // type: "GET",
-      url:
-        "https://apifree.forvo.com/key/50cc8c901d98b50a741856ded1071131/format/json/action/standard-pronunciation/word/cat",
-      data: "data",
-      dataType: "jsonp"
-    }).then(getAudio);
-  });
-
-  const getPinYin = data => {
-    console.log(data[0]);
-    console.log(data[0].hanzi);
-    console.log(data[0].pinyin);
-    console.log(data[0].translations[0]);
-  };
-
+  // const getAudio = data => {
+  //   $(".study-card")
+  //     .children("button")
+  //     .remove();
+  //   const $pronincuationButton = $("<button>")
+  //     .attr("class", "pronunciaion")
+  //     .attr("href", `${data.items[0].pathmp3}`)
+  //     .text(`${data.items[0].word}`);
+  //   $(".study-card").append($pronincuationButton);
+  // };
+  // $(".pronunciaion").on("click", () => {
+  //   $.ajax({
+  //     // type: "GET",
+  //     url:
+  //       "https://apifree.forvo.com/key/50cc8c901d98b50a741856ded1071131/format/json/action/standard-pronunciation/word/cat",
+  //     data: "data",
+  //     dataType: "jsonp"
+  //   }).then(getAudio);
+  // });
+  // const getPinYin = data => {
+  //   console.log(data[0]);
+  //   console.log(data[0].hanzi);
+  //   console.log(data[0].pinyin);
+  //   console.log(data[0].translations[0]);
+  // };
   // $.ajax({
   //   type: "GET",
   //   url: "/hanyu-shuiping-kaoshi/json/hsk-level-1.json",
   //   data: "data"
   // }).then(getPinYin);
-
   // modal pops up giving directions
   // modal pops up asking how many cards to study
   // set variable equal to number of cards
